c2bc0afd930d200727b25ec52167483203c90a58
hunglo67 <71253293+hunglo67@users.noreply.github.com>
2022-03-17 22:36:55 +0300

    Pr8-kis

---
+++ Kispython/B15-N8-Kispython.py
@@ -0,0 +1,21 @@
+import re
+
+S = '( <sect>var rima <| [q(atle_580) ;q(reso_670) ;q(vere_921) ]</sect>.\
+<sect> var raqu<| [ q(teonin); q(orrema) ] </sect>. <sect> var\
+xear_138 <| [ q(oranso_538) ; q(quso); q(biceis_141);q(ared_357)]\
+</sect>.)'
+def main(s):
+    D = {}
+    s = s.replace("\n", " ")
+    d = re.findall(r'<sect>(.*?)<\/sect>', s)
+    for i in d:
+        prt = re.sub(r'\D?var', string=i, repl='')
+        key = re.findall(r'\[(.*?)\]', prt)
+        for j in key:
+            keys = re.findall(r'\((.*?)\)',j)
+        values = re.findall(r'\s?(.*?)\s?<\D?|',prt)[0]
+        D[values] = keys
+    return D
+
+
+print(main(S))
